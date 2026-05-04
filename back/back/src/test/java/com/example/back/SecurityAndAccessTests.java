88d82d9418ef6e17d99798d476f421d10e9e42d7
Tomas Martinkus <tomreikalai111@gmail.com>
2026-05-05 01:10:12 +0300

    SGR-3 testas

--- back/back/src/test/java/com/example/back/SecurityAndAccessTests.java
+++ back/back/src/test/java/com/example/back/SecurityAndAccessTests.java
@@ -1,14 +1,21 @@
 package com.example.back;
 
+import com.example.back.auth.repo.AccountRepo;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -16,8 +23,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 class SecurityAndAccessTests {
+
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountRepo accountRepo;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     private String getJwtToken() throws Exception {
         String body = """
@@ -49,5 +63,26 @@ class SecurityAndAccessTests {
     void userShouldNotAccessUnauthorizedData() throws Exception {
         String token = getJwtToken();
         mockMvc.perform(get("/api/vehicles/999999/documents").header("Authorization", "Bearer " + token)).andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    @Transactional
+    @DisplayName("SGR-3: vartotojo slaptažodis duomenų bazėje turi būti saugomas maišos forma")
+    void passwordShouldBeStoredAsHash() throws Exception {
+        String email = "sgrtest@test.com";
+        String password = "tomas123";
+        String body = """
+        {
+          "email": "%s",
+          "password": "%s"
+        }
+        """.formatted(email, password);
+
+        mockMvc.perform(post("/api/auth/register").contentType(MediaType.APPLICATION_JSON).content(body)).andExpect(status().isOk());
+
+        var account = accountRepo.findByEmail(email).orElseThrow();
+
+        assertNotEquals(password, account.getPasswordHash());
+        assertTrue(passwordEncoder.matches(password, account.getPasswordHash()));
     }
 }
